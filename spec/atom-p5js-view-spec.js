79467a854ac73e33f5e2c4b957b3155b4c4256dc
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2020-03-27 10:59:16 +0100

    First commit

---
+++ spec/atom-p5js-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomP5jsView from '../lib/atom-p5js-view';
+
+describe('AtomP5jsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
